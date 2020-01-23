88a3da424348b83af2216a86a512a5d780b1346f
ky82 <barwa@123>
2020-01-22 23:20:36 -0800

    updated node files

---
+++ client/build/precache-manifest.9dacd7f8a0077e2600fbf2b10a36834e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60f5e14705af437227d0051982670b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a12a3802875eeb4cb997",
+    "url": "/static/css/main.2df4ed4a.chunk.css"
+  },
+  {
+    "revision": "d5af1d47a2a4f6b2a01d",
+    "url": "/static/js/2.1198c0ac.chunk.js"
+  },
+  {
+    "revision": "947d71d9788fd87037415f4aa5284067",
+    "url": "/static/js/2.1198c0ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a12a3802875eeb4cb997",
+    "url": "/static/js/main.c5c8b779.chunk.js"
+  },
+  {
+    "revision": "2543631445c241be0be8",
+    "url": "/static/js/runtime-main.e9074294.js"
+  },
+  {
+    "revision": "b87d541f816b9aafa5d38267c72e8ae7",
+    "url": "/static/media/logo.b87d541f.png"
+  }
+]);
