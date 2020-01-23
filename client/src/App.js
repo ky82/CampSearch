d25cc3f4b2bde47f323f59f4bdb1fe8155f1b25e
ky82 <barwa@123>
2020-01-22 22:20:07 -0800

    updated client folder

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer"
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+
+class App extends Component {
+
+  state = {
+    campgrounds: [],
+    loading: false,
+    name: "",
+    isLoggedIn: false,
+
+  }
+  handleLogin = (name, isLogged) => {
+    this.setState({
+      isLoggedIn: isLogged,
+    }, () => {
+      this.setState({
+        name: name
+      })
+      localStorage.setItem('name',
+      JSON.stringify(this.state.name));
+    })
+  }
+
+  handleCampgrounds = campgrounds =>
+    this.setState({ campgrounds: campgrounds })
+
+  handleLoading = loading =>
+    this.setState({ loading: loading })
+
+  render() {
+    return (
+      <Router>
+        <div id="app">
+          <Navbar isLoggedIn={this.state.isLoggedIn} handleLogin={this.handleLogin} />
+          <Header handleCampgrounds={this.handleCampgrounds} handleLoading={this.handleLoading} />
+          <Wrapper>
+            <Route path="/" render={props => <Home {...props} campgrounds={this.state.campgrounds} loading={this.state.loading} />} />
+            <Route path="/favorites" component={Favorites} />
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    )
+  };
+};
+
+export default App;
