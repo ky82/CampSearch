9e7e9acd4ad7d84eb77a24fa79f902a386c40007
ky82 <barwa@123>
2020-01-27 22:13:06 -0800

    updated server file

--- server.js
+++ server.js
@@ -3,16 +3,13 @@ const express = require("express");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-
-  app,get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  })
 }
 
 // Define API routes
