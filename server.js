f3fe472baa3f305c2a8f100666f3786985013855
ky82 <barwa@123>
2020-01-27 22:08:01 -0800

    updated server file

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const db = require("./db/models")
 const express = require("express");
-const path = require("path")
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-// Define API routes
-
-app.use("/", routes)
 
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
@@ -21,6 +14,10 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   })
 }
+
+// Define API routes
+
+app.use(routes)
 
 // Starts the server to begin listening and sync sequelize models
 // =============================================================
